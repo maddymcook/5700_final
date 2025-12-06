6cb8dea770d72917c523cf59e40d303905d592f1
Madison Cook <madison.cook@usu.edu>
2025-12-05 21:22:42 -0700

    Wire /about route to home controller

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -1,30 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const axios = require('axios');
-const homeController = require('../controllers/home-controller');
+const axios = require("axios");
+const homeController = require("../controllers/home-controller");
 
-// GET HOME PAGE 
-router.get('/', homeController.getHome);
+router.get("/", homeController.getHome);
 
-// GET externalapi 
-router.get('/externalapi', async (req, res, next) => {
+router.get("/about", homeController.getAbout);
+
+router.get("/externalapi", async (req, res, next) => {
   try {
-    const apiResponse = await axios.get('https://dog.ceo/api/breeds/image/random');
+    const apiResponse = await axios.get("https://dog.ceo/api/breeds/image/random");
     const dogImageUrl = apiResponse.data.message;
 
-    res.render('external-api', {
-      pageTitle: 'External API Demo',
-      pageClass: 'external-api-page',
+    res.render("external-api", {
+      pageTitle: "External API Demo",
+      pageClass: "external-api-page",
       dogImageUrl,
       errorMessage: null,
     });
   } catch (error) {
-    console.error('Error calling Dog CEO API:', error);
-    res.status(500).render('external-api', {
-      pageTitle: 'External API Demo',
-      pageClass: 'external-api-page',
+    console.error("Error calling Dog CEO API:", error);
+    res.status(500).render("external-api", {
+      pageTitle: "External API Demo",
+      pageClass: "external-api-page",
       dogImageUrl: null,
-      errorMessage: 'Sorry, could not load data from the external API.',
+      errorMessage: "Sorry, could not load data from the external API.",
     });
   }
 });
